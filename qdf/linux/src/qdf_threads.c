OnePlusOSS/android_kernel_oneplus_msm8998

qcacmn: Add qdf_print_thread_trace API
Add an abstraction to QDF for printing the stack trace of a given
thread.

Change-Id: Ibea6e6bed7f3ebe67538b8ea8b6b437b643d5541
CRs-Fixed: 2088806

qdf/linux/src/qdf_threads.c
@@ -40,6 +40,7 @@
 #include <linux/delay.h>
 #include <linux/interrupt.h>
 #include <linux/export.h>
+#include <stacktrace.h>
 
 /* Function declarations and documenation */
 
@@ -106,3 +107,27 @@ void qdf_busy_wait(uint32_t us_interval)
 	udelay(us_interval);
 }
 qdf_export_symbol(qdf_busy_wait);
+
+#if defined(CONFIG_MCL) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0))
+/* save_stack_trace_tsk is not generally exported for arm architectures */
+#define QDF_PRINT_TRACE_COUNT 32
+void qdf_print_thread_trace(qdf_thread_t *thread)
+{
+	const int spaces = 4;
+	struct task_struct *task = (struct task_struct *)thread;
+	unsigned long entries[QDF_PRINT_TRACE_COUNT] = {0};
+	struct stack_trace trace = {
+		.nr_entries = 0,
+		.skip = 0,
+		.entries = &entries[0],
+		.max_entries = QDF_PRINT_TRACE_COUNT,
+	};
+
+	save_stack_trace_tsk(task, &trace);
+	print_stack_trace(&trace, spaces);
+}
+#else
+void qdf_print_thread_trace(qdf_thread_t *thread) { }
+#endif /* CONFIG_MCL */
+qdf_export_symbol(qdf_print_thread_trace);
+

nekoshirro/android_kernel_xiaomi_sm6250

Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

qdf/linux/src/qdf_threads.c
@@ -44,6 +44,7 @@
 #include <linux/interrupt.h>
 #include <linux/export.h>
 #include <linux/kthread.h>
+#include <stacktrace.h>
 
 /* Function declarations and documenation */
 
@@ -129,3 +130,28 @@ int qdf_wake_up_process(qdf_thread_t *thread)
 	return wake_up_process(thread);
 }
 EXPORT_SYMBOL(qdf_wake_up_process);
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 13, 0)) ||\
+	defined(BACKPORTED_EXPORT_SAVE_STACK_TRACE_TSK)
+/* save_stack_trace_tsk is not generally exported for arm architectures */
+#define QDF_PRINT_TRACE_COUNT 32
+void qdf_print_thread_trace(qdf_thread_t *thread)
+{
+	const int spaces = 4;
+	struct task_struct *task = thread;
+	unsigned long entries[QDF_PRINT_TRACE_COUNT] = {0};
+	struct stack_trace trace = {
+		.nr_entries = 0,
+		.skip = 0,
+		.entries = &entries[0],
+		.max_entries = QDF_PRINT_TRACE_COUNT,
+	};
+
+	save_stack_trace_tsk(task, &trace);
+	print_stack_trace(&trace, spaces);
+}
+#else
+void qdf_print_thread_trace(qdf_thread_t *thread) { }
+#endif /* KERNEL_VERSION(4, 13, 0) */
+EXPORT_SYMBOL(qdf_print_thread_trace);
+