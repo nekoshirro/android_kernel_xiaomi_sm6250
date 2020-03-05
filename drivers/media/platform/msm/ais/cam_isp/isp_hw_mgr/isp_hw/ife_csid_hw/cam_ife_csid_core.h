UtsavBalar1231/kernel_xiaomi_raphael

msm: ais: handle timestamp for each RDI of IFE
There are some resource conflicts when handle timestamps of
multi RDIs on the same IFE. need allocate variable to save
timestamp for each RDI path.

Change-Id: Ibd765fe0e9803ea49ca0209c4775851c464d1afa
Signed-off-by: YUE CHEN <yueche@codeaurora.org>

drivers/media/platform/msm/ais/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.h
@@ -564,8 +564,8 @@ struct cam_ife_csid_hw {
 	uint32_t                         dual_usage;
 	uint32_t                         init_frame_drop;
 	uint32_t                         res_sof_cnt[CAM_IFE_PIX_PATH_RES_MAX];
-	uint64_t                         prev_boot_timestamp;
-	uint64_t                         prev_qtimer_ts;
+	uint64_t                         prev_boot_timestamp[CAM_IFE_CSID_RDI_MAX];
+	uint64_t                         prev_qtimer_ts[CAM_IFE_CSID_RDI_MAX];
 	struct cam_hw_intf              *ppi_hw_intf[CAM_CSID_PPI_HW_MAX];
 	bool                             ppi_enable;
 	bool                             fatal_err_detected;

nekoshirro/android_kernel_xiaomi_sm6250

drivers/media/platform/msm/ais/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -564,8 +564,8 @@ struct cam_ife_csid_hw {
 	uint32_t                         dual_usage;
 	uint32_t                         init_frame_drop;
 	uint32_t                         res_sof_cnt[CAM_IFE_PIX_PATH_RES_MAX];
-	uint64_t                         prev_boot_timestamp;
-	uint64_t                         prev_qtimer_ts;
+	uint64_t             prev_boot_timestamp[CAM_IFE_PIX_PATH_RES_MAX];
+	uint64_t             prev_qtimer_ts[CAM_IFE_PIX_PATH_RES_MAX];
 	struct cam_hw_intf              *ppi_hw_intf[CAM_CSID_PPI_HW_MAX];
 	bool                             ppi_enable;
 	bool                             fatal_err_detected;